024fbe52657a213d28a19211353ac528dd763cb4
balachandar <govindalapudibalachandar@gmail.com>
2021-10-05 23:08:13 +0530

    Added:CalculateFare file is added

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
 
+//Class Invoice Generator
 public class InvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome to cab Invoice generator");
+
+        //Declare Constant Variables
+        private static final double MINIMUM_COST_PER_KM = 10.0;
+        private static final int COST_PER_TIME = 1 ;
+        private static final double MINIMUM_FARE = 5;
+
+        //Method to Calculate Fare
+        public double calculateFare(double distance, int time) {
+            double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME ;
+            if (totalFare < MINIMUM_FARE)
+                return MINIMUM_FARE;
+            return totalFare;
+        }
     }
-}
